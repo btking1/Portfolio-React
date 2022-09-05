85c46339705f66a779ec80007e5f93f93d691cee
Booker King <btking1@my.waketech.edu>
2022-09-05 02:28:53 -0400

    Updates

--- precache-manifest.d3d742d09557da4ec83399a1297b0542.js
+++ precache-manifest.d3d742d09557da4ec83399a1297b0542.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c29ecd927a9ac30ec8dc",
-    "url": "/Portfolio-React/static/css/main.88bda876.chunk.css"
+    "revision": "f3ac954a2da7e2c894f6",
+    "url": "/Portfolio-React/static/css/main.7f229ed6.chunk.css"
   },
   {
-    "revision": "c29ecd927a9ac30ec8dc",
-    "url": "/Portfolio-React/static/js/main.c29ecd92.chunk.js"
+    "revision": "f3ac954a2da7e2c894f6",
+    "url": "/Portfolio-React/static/js/main.f3ac954a.chunk.js"
   },
   {
     "revision": "b56410e07f342136e5de",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Portfolio-React/static/js/runtime~main.d68143df.js"
   },
   {
-    "revision": "ac9e83ec92660ae15e7bd72ee55d7917",
+    "revision": "c7cbfd8fee7f5bf19aa67e25c94d2c48",
     "url": "/Portfolio-React/index.html"
   }
 ];
