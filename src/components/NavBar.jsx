19bee0d744dc5a393220d8de93f415b417c75856
Booker King <btking1@my.waketech.edu>
2022-09-05 22:34:50 -0400

    nav styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 import {
@@ -7,16 +7,33 @@ import {
   Box,
   AppBar,
   IconButton,
+  Button,
   Typography,
   makeStyles,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   logoLg: {
+    width: 'fit-content',
     display: "block",
-    [theme.breakpoints.down("sm")]: {},
+    padding: "0.5rem 1rem",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    fontFamily: "Dosis",
+    // [theme.breakpoints.down("sm")]: {},
   },
   logoSm: {},
+  navContainer: {
+    flexGrow: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  linkContainer: {
+    width: "fit-content",
+    display: "block",
+    justifyContent: "space-between",
+  }
 }));
 
 function NavBar() {
@@ -58,16 +75,45 @@ function NavBar() {
   //   `;
   const classes = useStyles();
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-      }}
-    >
-      <AppBar>
-        <Typography variant="h4" className={classes.logoLg}>BK :</Typography>
-        <Typography variant="h4">Dev</Typography>
+    // <Box sx={{
+    //   width: "100%",
+    // }}>
+      <AppBar className={classes.navContainer}>
+        <NavLink key='5' to='/'>
+        <Typography variant="h4" component="div" className={classes.logoLg}>
+          BK : DEV
+        </Typography>
+        </NavLink>
+        <div className={classes.linkContainer}>
+          {[
+            {
+              childText: `About`,
+              key: 1,
+              link: `/about`,
+            },
+            {
+              childText: `Works`,
+              key: 2,
+              link: `/works`,
+            },
+            {
+              childText: `Contact`,
+              key: 3,
+              link: `/contact`,
+            },
+            {
+              childText: `Resume`,
+              key: 4,
+              link: `/resume`,
+            },
+          ].map((data) => (
+            <NavLink key={data.key} to={data.link}>
+              <Button key={data.key}>{data.childText}</Button>
+            </NavLink>
+          ))}
+        </div>
       </AppBar>
-    </Box>
+    // </Box>
     // <Container maxWidth='xl'
     //   sx={{
     //     display: "flex",
